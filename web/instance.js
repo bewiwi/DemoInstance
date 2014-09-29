21fd0323ae8b54c606bc6a49ae1cbfe765f079a4
jubianchi <julien.bianchi@pmsipilot.com>
2014-09-29 10:57:19 +0200

    Modification du InstanceController

--- web/instance.js
+++ web/instance.js
@@ -1,6 +1,30 @@
-function Instance($scope,$http) {
-    $http.get('/instance/start').
-        success(function(data) {
-            $scope.cloud_instance = data;
-        });
-}
+demoApp.controller('instanceController', function($scope, $http, $interval) {
+    var errorCallback = function(error) {
+        $scope.error = error;
+    };
+
+    $scope.createInstance = function() {
+        $scope.cloud_instance = {};
+
+        $http.get('/instance/start').
+            success(function(data) {
+                $scope.cloud_instance = data;
+
+                var refreshInterval = $interval(
+                    function() {
+                        $http.get('/instance/' + $scope.cloud_instance.id).
+                            success(function(data) {
+                                $scope.state = data;
+
+                                if ($scope.state.demo_address) {
+                                    $interval.cancel(refreshInterval);
+                                }
+                            }).
+                            error(errorCallback);
+                    },
+                    15000
+                );
+            }).
+            error(errorCallback);
+    };
+});
